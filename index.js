961019907dea748dc952935eefaeb596b50e983e
Tommy Daniels <thomsondaniels@hotmail.com>
2021-07-10 08:58:27 +1000

    misc - refactor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+
 // new folder absolute path
 const RELATIVE_PKG_PATH = './package.json';
 const packageJsonPath = path.join(process.cwd(), RELATIVE_PKG_PATH);
@@ -8,15 +9,25 @@ const packageJsonPath = path.join(process.cwd(), RELATIVE_PKG_PATH);
 const pkg = require(packageJsonPath);
 const { version: pkgVersion } = pkg;
 
-const flow = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
-const dropLast = array => array.slice(0, -1);
-const join = array => array.join('.');
+const flow = (...fns) => (arg) => fns.reduce((v, f) => f(v), arg);
 
-const splitParts = string => string.split('.');
-const toNumbers = array => array.map((string) => Number(string));
+const splitParts = string => string.split('.'); // 'x.x.x' into ['x','x','x']
 const getLast = array => array.pop();
 const toNumber = number => parseInt(number);
 const bump = number => number + 1;
+
+const toNumbers = array => array.map(toNumber);
+const dropLast = array => array.slice(0, -1);
+const join = array => array.join('.');
+
+// increase the last "number" from the string "x.x.x"
+// => number (x + 1)
+const pseudoPatch = flow(
+  splitParts,
+  getLast,
+  toNumber,
+  bump
+);
 
 // removes the last "number" from the string "x.x.x"
 // => string ('x.x')
@@ -25,15 +36,6 @@ const strip = flow(
   toNumbers,
   dropLast,
   join,
-);
-
-// increase the last "number" from the string "x.x.x"
-// => number x + 1
-const pseudoPatch = flow(
-  splitParts,
-  getLast,
-  toNumber,
-  bump
 );
 
 const version = `${strip(pkgVersion)}.${pseudoPatch(pkgVersion)}`; // === 'x.x' + '.x'
