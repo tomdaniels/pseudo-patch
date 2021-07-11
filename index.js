f1700b8720fbd8291e7fe0a79c624d6540591544
Tommy Daniels <thomsondaniels@hotmail.com>
2021-07-11 10:54:51 +1000

    refactor opts definition

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const { Command } = require('commander');
 const pseudoPatch = require('./lib/pseudo-patch');
 
 const cli = new Command();
-cli.option(
-  '-s, --spacing <type>',
-  'preferred indent spacing size',
-);
+const opts = [
+  {
+    flags: '-s, --spacing <type>',
+    desc: 'preferred indent spacing size',
+    // this handler is not required for all opts
+    // commander parses all args as strings
+    handler: (val) => parseInt(val, 10)
+  },
+  // extend me..
+];
+opts.forEach((opt) => cli.option(
+  opt.flags,
+  opt.desc,
+  opt.handler,
+));
 
 cli.parse(process.argv);
 pseudoPatch(cli.opts());
